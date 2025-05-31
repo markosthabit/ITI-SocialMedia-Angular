831a38814027a044fefa1b6634a7a462da6d7e39
markosthabit <markosthabit@protonmail.com>
2025-06-01 01:08:30 +0300

    💄 feat: Add post to the app.html for display. > ➕feat: Add Post component to the component directive in app.ts > ♻️refactor:  Change the Posts class property name in posts.ts and posts.html

--- src/app/components/post/post.ts
+++ src/app/components/post/post.ts
@@ -9,5 +9,9 @@ import { Community } from '../../models/community';
 })
 
 export class Post {
-  postCommunity: Community = new Community("communityName", "https://img.pikbest.com/png-images/20250510/community-network-and-social-icon-design-template-logo-vector_11708048.png!w700wp", ["subgroup1", "subgroup2"], [])
+
+  communityProperty: Community = new Community("communityName",
+    "https://img.pikbest.com/png-images/20250510/community-network-and-social-icon-design-template-logo-vector_11708048.png!w700wp",
+    ["subgroup1", "subgroup2"],
+    [])
 }
