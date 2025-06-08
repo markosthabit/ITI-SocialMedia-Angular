b56c02c8f12815c1bfb312499606aed8f44c855f
markosthabit <markosthabit@protonmail.com>
2025-06-08 20:07:38 +0300

    💄feat(community): display timestamp for each post, and likes using their respective pipes.

--- src/app/components/PostBoard/PostBoard.component.ts
+++ src/app/components/PostBoard/PostBoard.component.ts
@@ -2,10 +2,11 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { IPost } from '../../models/ipost';
 import { CommonModule } from '@angular/common';
 import { PostStyleDirective } from '../../directives/postStyle.directive';
+import { FormatLikesPipe } from "../../pipes/format-likes-pipe";
 
 @Component({
   selector: 'app-PostBoard',
-  imports: [CommonModule, PostStyleDirective],
+  imports: [CommonModule, PostStyleDirective, FormatLikesPipe],
   templateUrl: './PostBoard.component.html',
   styleUrls: ['./PostBoard.component.css']
 })
